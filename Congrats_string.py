7a5ba5236faf6c2d586ee3c7a77f684ede328701
ZaRyAaB0419 <kinghereah@gmail.com>
2024-09-29 16:22:54 +0500

    Add files via upload

---
+++ Congrats_string.py
@@ -0,0 +1,10 @@
+# adding input
+my_string=str(input("Write Your Letter information/Topic:"))
+
+#Converting the lower to upper
+uppercase_string=my_string.upper()
+
+#printing the uppercase
+print("Your Title in uppercase:", uppercase_string)
+
+print("You are good to go")
